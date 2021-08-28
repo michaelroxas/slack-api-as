f79365c297aee39a88d8ba1560d1b278f0d3a76b
Michael Roxas <work.michaelroxas@gmail.com>
2021-08-28 12:43:43 +0800

    Delete default layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, Van!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <section>something here</section>;
 }
 
 export default App;
